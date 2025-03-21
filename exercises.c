3f316804e11325bf6bba8db9734ee7df57215f09
Your Name <you@example.com>
2025-03-21 14:38:44 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -81,7 +81,7 @@ void eliminaElementos(List *L, int elem) {
   for (int i = 0; i < talla; i++) {
     if (*dato == elem) {
       popCurrent(L);
-      int *dato = (int*) next(L);
+      dato = (int*) next(L);
     }
   }
 }
